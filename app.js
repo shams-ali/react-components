57ff7f2d7d4f7c0461da9818808e9ce126a02e18
Shams Ali <ali.s90@outlook.com>
2016-07-27 15:21:52 -0500

    updated

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+
+
+// var Grocerylist = (props) => {
+//   var onListItemClick = (event) => {
+//     console.log('i got clicked');
+//   }
+//   return (
+//     <ul>
+//       <li onClick = {onListItemClick} > {props.groceries[0]}</li>
+//       <li>{props.groceries[1]}</li>
+//     </ul>
+//   )
+// };
+//
+//
+//
+//
+
+
+class GroceryListItem extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <li>{this.props.list.grocery}</li>
+    );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.groceries.map(grocery =>
+    <GroceryListItem key = {grocery.thing} list={grocery}/>
+  )}
+  </ul>
+);
+
+const myGroceries = [{grocery: 'milk' , grocery: ''}]
+// var App = () => (
+//   <div>
+//     <h2> My Grocery List </h2>
+//     <Grocerylist groceries = { ['Oranges', 'Apples'] } />
+//   </div>
+// );
+
+ReactDOM.render(<GroceryListItem />, document.getElementById('app'));
